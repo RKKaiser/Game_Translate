2db20edc0a744831c75892c7772aff8f46a9d7e0
sui_de_fan <3188571162@qq.com>
2026-04-13 10:50:23 +0800

    feat: 在DataManager中添加段位和VIP等级变化逻辑

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -5,22 +5,31 @@ using UnityEngine;
 public class DataManager : MonoBehaviour
 {
     public static DataManager Instance;
-    public int gameT;   //游戏游玩次数
-    public int topUpT;  //充值次数
 
-    public int VIPRank;
-
-    public int rank;
-
+    public int gameT; // 游戏游玩次数
+    public int topUpT; // 充值次数
+    public int VIPRank; 
+    public int rank; 
     public int pet;
-
     public int inscription;
 
+    // 新增：用于标记充值状态，判断是否是充值后的第一局
+    private bool hasJustTopUp = false;
 
     void Awake()
     {
-        DontDestroyOnLoad(this.gameObject);
+        // 单例模式初始化
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -31,5 +40,28 @@ public class DataManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    // 新增方法：充值逻辑
+    public void OnPlayerTopUp()
+    {
+        topUpT += 1;
+        
+        // 逻辑1：每次充值 VIPRank + 1
+        VIPRank += 1;
+        
+        // 逻辑2：标记为刚刚充值状态，等待下一局游戏触发段位提升
+        hasJustTopUp = true;
+    }
+
+    // 新增方法：游戏结算逻辑（需在每局游戏结束时调用）
+    public void OnGameEnd()
+    {
+        // 如果是充值后的第一局游戏
+        if (hasJustTopUp)
+        {
+            rank += 1; // 段位+1
+            hasJustTopUp = false; // 重置标记
+        }
     }
 }
